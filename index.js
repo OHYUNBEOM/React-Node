a01f847c3076c8dab87bc9d19abff8d5adb68cb7
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-08-01 00:06:33 +0900

    login

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 5000 // localhost:5000
 const bodyParser= require('body-parser');//npm install body-parser 로 받아온 body-parser 을 불러옴
 const { User }= require("./models/User"); // models 의 User.js 파일을 불러옴
-
+const cookieParser=require('cookie-parser');
 const config=require('./config/key');
 
 // //application/x-www-form-urlencoded 로 넘어오는 정보를 분석해주기 위해 필요함
@@ -15,7 +15,7 @@ const config=require('./config/key');
 app.use(express.urlencoded({extended:true}));
 //application/json --> json 형태로 넘어오는걸 받기위해서 필요함
 app.use(express.json());
-
+app.use(cookieParser());
 
 const mongoose = require('mongoose');//NoSQL
 mongoose.connect(config.mongoURI,{
@@ -42,7 +42,7 @@ app.post('/register',(req,res) => {
 
 app.post('/login',(req,res)=>{
     //요청된 이메일을 DB에서 찾는다
-    User.findOne({ email:req.body.email},(err,user)=>{
+    User.findOne({email:req.body.email},(err,user)=>{
         if(!user)
         {
             return res.json({
@@ -50,15 +50,19 @@ app.post('/login',(req,res)=>{
                 message:"제공된 이메일에 해당하는 유저가 없습니다."
             })
         }
-    })
     //이메일이 있다면 비밀번호가 같은지 확인
     user.comparePassword(req.body.password,(err,isMatch)=>{
         if(!isMatch)
-        return res.json({loginSuccess:false,message:"비밀번호가 틀렸습니다."})
+            return res.json({loginSuccess:false,message:"비밀번호가 틀렸습니다."})
     })
     //비밀번호 까지 같다면 User를 위한 Token 을 생성
     user.generateToken((err,user)=>{
-        
+        if(err) return res.status(400).send(err);//statue(400)이면 error 가 있다는 의미
+        //user 에서 받아온 token 을 쿠키에 저장
+        res.cookie("x_auth",user.token)//x_auth 라는 이름의 cookie 에 user에 삽입된 token 이 저장됨
+            .status(200)//성공했을때
+            .json({loginSuccess:true,userId: user._id})//userId 를 user._id 로 
+        })
     })
 })
 
