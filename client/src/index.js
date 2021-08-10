44ace3028d29ad09781cc9155b35264dec06d0cf
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-08-10 22:23:03 +0900

    modifying

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+import { Provider } from 'react-redux';
+
+
+import 'antd/dist/antd.css';
+import { applyMiddleware,createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers/';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore)
+
+
+ReactDOM.render( 
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__&&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+  >
     <App /> 
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>
+
+  
+  ,document.getElementById('root')); 
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />  // 여기 부분에 보여주고싶은 컴포넌트를 넣는다 (App / 이 아닌 <div>Hello</div>) 작성시에 화면에 Hello 만 보여짐
 //   </React.StrictMode>,
 //   document.getElementById('root')
-// );
+// ); 
 
 
 // If you want to start measuring performance in your app, pass a function
