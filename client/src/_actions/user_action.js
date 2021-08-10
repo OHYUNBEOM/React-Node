c0f7ee4effec083fc3a45516cda573c423c4a99c
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-08-10 22:23:25 +0900

    Login

---
+++ client/src/_actions/user_action.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import {
+    LOGIN_USER
+}from './types';
+export function loginUser(dataToSubmit){
+    const request=axios.post('/api/users/login',dataToSubmit) // 입력한 email 과 password 를 받아와서 body 에다 받고, 그 body 를 axios 를 통해서 .post(서버로 보내줌)
+        .then(response => response.data)//서버에서 받은 데이터를 request 에 저장한다
+    
+    //server 부분 index.js 에 만들어놓은 login,register,logout 등등 사용
+    return{
+        type: "LOGIN_USER",
+        payload: request//request 를 payload 에 넣어서 request 가 가져온 모든 data 이기에 모든 데이터가 redux 에 들어감
+    }
+}
