db17401512ad2bdb685dfd6cfe7cd5106fbbf771
OHYUNBEOM <dbsqja353@naver.com>
2021-08-11 00:15:58 +0900

    Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+import LandingPage from './components/views/Landingpage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+
+//Routing 기능 , 어떻게 페이지가 클릭시에 이동되는지 Routing으로 구현
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Starting React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/*
+          A <Switch> looks through all its c hildren <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/" component={LandingPage} /> 
+          {/* component={?} --> ?가 LandingPage 로 보내주는것을 의미  */}
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
